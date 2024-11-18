5e6f25f177a57c0aad66fbc36f2c1b01a0b1a4b4
shayneintsu <shayneintsu@gmail.com>
2024-11-18 14:19:00 +0100

    static scripting init

---
+++ scripts/my-script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Create a new file called "new_file.txt"
+touch new_file.txt
+
+# If you want to add content to the file, you can use the following command:
+echo "This is the content of the new file." > new_file.txt
