f2f5ee5bf5f967cd0898cd91d55a1caf60185439
shayneintsu <shayneintsu@gmail.com>
2024-11-21 15:42:21 +0100

    j ai oublie les majs

--- scripts/myscript.sh
+++ scripts/myscript.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed -i 's/⚪/$(printf "\\U$(shuf -i 0x1F7E8-0x1F7EB -n 1 | tr -d '\n')")/g' ../readme.md
+sed -i 's/⚪/$(printf "\\U$(shuf -i 0x1F7E8-0x1F7EB -n 1 | tr -d '\n')")/g' ../README.md
 
