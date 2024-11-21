d312ffef15e68551026908cd6c98190efdc0002f
shayneintsu <shayneintsu@gmail.com>
2024-11-22 00:39:36 +0100

    and the . and ?

--- scripts/draw.sh
+++ scripts/draw.sh
@@ -3,5 +3,5 @@
 cell_number=$1
 
 color=$(cat assets/color.txt) 
-sed -i "s/\[[\u26aa-\u2b50]\](https:\/\/shayneintsu.github.io\/DrawMeSomething.github.io?cell=$cell_number)/\[$color\](https:\/\/shayneintsu.github.io\/DrawMeSomething.github.io?cell=$cell_number)/g" "./README.md"
+sed -i "s/\[[\u26aa-\u2b50]\](https:\/\/shayneintsu\.github\.io\/DrawMeSomething\.github\.io\?cell=$cell_number)/\[$color\](https:\/\/shayneintsu\.github\.io\/DrawMeSomething\.github\.io\?cell=$cell_number)/g" "./README.md"
 
--- scripts/select.sh
+++ scripts/select.sh
@@ -39,7 +39,7 @@ if [[ $cell_number -ge -9 && $cell_number -le -1 ]]; then
   square=$(cell_to_square "$cell_number")
   round=$(cell_to_round "$cell_number")
   echo "$square" > "./assets/color.txt"
-  sed -i "s/\[$square\](https:\/\/shayneintsu.github.io\/DrawMeSomething.github.io?cell=$cell_number)/\[$round\](https:\/\/shayneintsu.github.io\/DrawMeSomething.github.io?cell=$cell_number)/g" "./README.md"
+  sed -i "s/\[$square\](https:\/\/shayneintsu\.github\.io\/DrawMeSomething\.github\.io\?cell=$cell_number)/\[$round\](https:\/\/shayneintsu\.github\.io\/DrawMeSomething\.github\.io\?cell=$cell_number)/g" "./README.md"
 else
   echo "Invalid cell number. Please enter a number between -1 and -9."
 fi
