554c03f80bdc0ac73739bc8bed0483b47a131bc7
shayneintsu <shayneintsu@gmail.com>
2024-11-22 22:16:43 +0100

    try again

--- scripts/draw.sh
+++ scripts/draw.sh
@@ -3,5 +3,5 @@
 cell_number=$1
 
 color=$(cat assets/color.txt) 
-sed -i "s/\[[\u2B1B\uD83D\uDFEB\uD83D\uDFEA\uD83D\uDFE9\uD83D\uDFE6\uD83D\uDFE8\uD83D\uDFE7\uD83D\uDFE5\u2B1C]\](https:\/\/shayneintsu.github.io\/DrawMeSomething.github.io?cell=$cell_number)/\[$color\](https:\/\/shayneintsu.github.io\/DrawMeSomething.github.io?cell=$cell_number)/g" "./README.md"
+sed -i "s/\[[⬜⬛🟫🟪🟦🟩🟨🟧🟥]\](https:\/\/shayneintsu.github.io\/DrawMeSomething.github.io?cell=$cell_number)/\[$color\](https:\/\/shayneintsu.github.io\/DrawMeSomething.github.io?cell=$cell_number)/g" "./README.md"
 
